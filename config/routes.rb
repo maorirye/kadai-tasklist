5976c0e4f9b6d5ae0b3822e1dad5ed69f60c48e9
kaito_kuroba <maorirye@gmail.com>
2019-01-19 09:35:09 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
- root to: 'tasks#index'
+  get 'toppages/index'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+ root to: 'toppages#index'
  
- resources :tasks
+ get 'logged_in', to: 'sessions#new'
+ post 'login', to:'sessions#create'
+ delete 'logout', to:'sessions#destroy'
+ 
+ get 'signup', to: 'users#new'
+ resources :users, only: [:index, :show, :new, :create]
 end
