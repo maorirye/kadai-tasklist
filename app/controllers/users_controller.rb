c56081b601004df097186738f078fcf9909b65e3
kaito_kuroba <maorirye@gmail.com>
2019-02-02 08:21:59 +0000

    revision

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,6 @@ class UsersController < ApplicationController
     @users = User.all.page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order('created_at DESC').page(params[:page])
-    counts(@user)
-  end
 
   def new
     @user = User.new 
